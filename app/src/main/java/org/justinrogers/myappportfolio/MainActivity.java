07fcaf4a0afa38be868eb0375eae454c3b4866b7
yaskyj <yaskyj@yahoo.com>
2015-06-05 23:28:54 -0500

    Add toast for each button

--- app/src/main/java/org/justinrogers/myappportfolio/MainActivity.java
+++ app/src/main/java/org/justinrogers/myappportfolio/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +38,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createToast(View view) {
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my " + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast.makeText(context, text, duration).show();
     }
 }
