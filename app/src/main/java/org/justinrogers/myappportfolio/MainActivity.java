6d6ee9f79631dcb9541b8896547e39f13ed67d9b
yaskyj <yaskyj@yahoo.com>
2015-08-02 09:13:33 -0500

    refactor: Add Butter Knife

--- app/src/main/java/org/justinrogers/myappportfolio/MainActivity.java
+++ app/src/main/java/org/justinrogers/myappportfolio/MainActivity.java
@@ -1,10 +1,8 @@
 package org.justinrogers.myappportfolio;
 
-import android.app.ActionBar;
-import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,7 +11,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Log.d(LOG_TAG, "Spotify");
+                createToast(view);
+                break;
+        }
+    }
     public void createToast(View view) {
         Button b = (Button)view;
         String buttonText = b.getText().toString();
